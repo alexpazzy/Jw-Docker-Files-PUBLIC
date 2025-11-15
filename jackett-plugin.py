1f88cbf14d563c30edf23c46a0c250263d9e80ab
Alex Paz <95468190+alexpazzy@users.noreply.github.com>
2025-11-14 16:58:31 -0800

    Update API key and Jackett URL in configuration

--- jackett-plugin.py
+++ jackett-plugin.py
@@ -55,8 +55,8 @@ proxy_manager.enable_proxy(False)  # off by default
 CONFIG_FILE = 'jackett.json'
 CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), CONFIG_FILE)
 CONFIG_DATA: Dict[str, Any] = {
-    'api_key': 'YOUR_API_KEY_HERE',  # jackett api
-    'url': 'http://127.0.0.1:9117',  # jackett url
+    'api_key': 'oyl62sjecv6mpt6pc07osf9966kskwl9',  # jackett api
+    'url': 'http://jackett:9117',  # jackett url
     'tracker_first': False,          # (False/True) add tracker name to beginning of search result
     'thread_count': 20,              # number of threads to use for http requests
 }
@@ -133,7 +133,7 @@ class jackett:
             return
 
         # check api_key
-        if self.api_key == "YOUR_API_KEY_HERE":
+        if self.api_key == "oyl62sjecv6mpt6pc07osf9966kskwl9":
             self.handle_error("api key error", what)
             return
 
